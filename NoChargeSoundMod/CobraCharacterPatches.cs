77d8546a5f6aab1611ecf428d2168c63355278a0
Lee23 <31892011+LeeTwentyThree@users.noreply.github.com>
2025-08-28 22:44:10 -0400

    Add No Charge Sound mod

---
+++ NoChargeSoundMod/CobraCharacterPatches.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Reflection.Emit;
+using HarmonyLib;
+
+namespace NoChargeSound;
+
+[HarmonyPatch(typeof(CobraCharacter), nameof(CobraCharacter.UpdateChargedShotHoldTimer))]
+public static class CobraCharacterPatches
+{
+    private const audioSelectionData.eCLIP SoundToReplace = audioSelectionData.eCLIP.PLY_SHOOT_PG_CHARGING;
+    
+    private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+    {
+        var codeMatcher = new CodeMatcher(instructions);
+
+        codeMatcher.Start()
+            .MatchForward(true, new CodeMatch(OpCodes.Ldc_I4, (int)SoundToReplace))
+            .ThrowIfInvalid("Failed to found the referenced sound!")
+            .Advance(2)
+            .Insert(CodeInstruction.Call(typeof(CobraCharacterPatches), nameof(MultiplyVolume)));
+        
+        return codeMatcher.Instructions();
+    }
+
+    private static float MultiplyVolume(float originalVolume)
+    {
+        return originalVolume * Plugin.SoundVolumeMultiplier.Value;
+    }
+}
---
+++ NoChargeSoundMod/Plugin.cs
@@ -0,0 +1,30 @@
+﻿using System.Reflection;
+using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using HarmonyLib;
+
+namespace NoChargeSound;
+
+[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+public class Plugin : BaseUnityPlugin
+{
+    internal static new ManualLogSource Logger;
+
+    public static ConfigEntry<float> SoundVolumeMultiplier;
+    
+    private void Awake()
+    {
+        Logger = base.Logger;
+
+        SoundVolumeMultiplier = Config.Bind("General",
+            "SoundVolumeMultiplier",
+            0f,
+            "The volume multiplier for the charge sound."
+        );
+
+        Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly());
+
+        Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+    }
+}
