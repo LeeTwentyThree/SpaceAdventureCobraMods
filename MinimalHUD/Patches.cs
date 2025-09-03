a60f3d3eb3c770592be848b11719c911dabc83b4
Lee23 <31892011+LeeTwentyThree@users.noreply.github.com>
2025-09-02 22:58:12 -0400

    Add minimal HUD mod

---
+++ MinimalHUD/Patches.cs
@@ -0,0 +1,23 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace MinimalHUD;
+
+[HarmonyPatch]
+public static class Patches
+{
+    [HarmonyPrefix]
+    [HarmonyPatch(typeof(UIController), nameof(UIController.AddGuidedShotTarget))]
+    public static bool RemoveGuidedShotTarget()
+    {
+        return false;
+    }
+    
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(NUIHUD), nameof(NUIHUD.Start))]
+    public static void HideHUD(NUIHUD __instance)
+    {
+        var canvasGroup = __instance.gameObject.GetComponent<CanvasGroup>();
+        canvasGroup.alpha = 0f;
+    }
+}
