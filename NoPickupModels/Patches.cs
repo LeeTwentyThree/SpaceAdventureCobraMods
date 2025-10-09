f1ec1f2d6a74617813f6471564ecfd6740b03e60
Lee23 <31892011+LeeTwentyThree@users.noreply.github.com>
2025-10-09 02:55:36 -0400

    Add no pickup models mod

---
+++ NoPickupModels/Patches.cs
@@ -0,0 +1,25 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace NoPickupModels;
+
+[HarmonyPatch]
+public static class Patches
+{
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(NmiDropCollectible), nameof(NmiDropCollectible.Awake))]
+    public static void AwakePostfix(NmiDropCollectible __instance)
+    {
+        foreach (var renderer in __instance.GetComponentsInChildren<Renderer>())
+        {
+            renderer.enabled = false;
+        }
+    }
+    
+    [HarmonyPrefix]
+    [HarmonyPatch(typeof(CobraCharacter), nameof(CobraCharacter.DropPickedup))]
+    public static bool DisablePickUpEffect()
+    {
+        return false;
+    }
+}
---
+++ NoPickupModels/Plugin.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+using System.Reflection;
+using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using UnityEngine;
+
+namespace NoPickupModels;
+
+[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+public class Plugin : BaseUnityPlugin
+{
+    internal static new ManualLogSource Logger;
+
+    internal static Texture2D NewTexture { get; private set; }
+
+    private void Awake()
+    {
+        Logger = base.Logger;
+
+        var assembly = Assembly.GetExecutingAssembly();
+        Harmony.CreateAndPatchAll(assembly);
+
+        Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+    }
+}
