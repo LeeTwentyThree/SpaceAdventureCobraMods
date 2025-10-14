29fe03fc1a959759368b20bd22b00c5b67cc9e2b
Lee23 <31892011+LeeTwentyThree@users.noreply.github.com>
2025-10-13 23:15:12 -0400

    Add Revolver Fix mod

---
+++ RevolverFix/Patches.cs
@@ -0,0 +1,35 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace RevolverFix;
+
+[HarmonyPatch]
+public static class Patches
+{
+    private static readonly int FloatMetalness = Shader.PropertyToID("_Float_metalness");
+    private static readonly int AlbedoTextureProperty = Shader.PropertyToID("_Texture2D_alb");
+    private const string CobraMaterialName = "mat_chr_Cobra_00";
+    
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(CobraCharacter), nameof(CobraCharacter.Start))]
+    public static void FixRevolverPatch(CobraCharacter __instance)
+    {
+        foreach (var material in __instance.defaultMats)
+        {
+            if (material != null && material.name == CobraMaterialName)
+            {
+                material.SetFloat(FloatMetalness, 0);
+                if (Plugin.NewTexture != null)
+                {
+                    material.SetTexture(AlbedoTextureProperty, Plugin.NewTexture);
+                }
+                else
+                {
+                    Plugin.Logger.LogError("Failed to find new cobra texture!");
+                }
+                return;
+            }
+        }
+        Plugin.Logger.LogWarning("Material not found!");
+    }
+}
---
+++ RevolverFix/Plugin.cs
@@ -0,0 +1,45 @@
+﻿using System.IO;
+using System.Reflection;
+using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using UnityEngine;
+
+namespace RevolverFix;
+
+[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+public class Plugin : BaseUnityPlugin
+{
+    internal static new ManualLogSource Logger;
+
+    internal static Texture2D NewTexture { get; private set; }
+
+    private void Awake()
+    {
+        Logger = base.Logger;
+
+        var assembly = Assembly.GetExecutingAssembly();
+        Harmony.CreateAndPatchAll(assembly);
+
+        // LOAD TEXTURE
+        
+        var modFolder = Path.GetDirectoryName(assembly.Location);
+        var planetFilePath = Path.Combine(modFolder, "NewCobraTexture.png");
+
+        byte[] fileData = File.ReadAllBytes(planetFilePath);
+        var texture = new Texture2D(2, 2, TextureFormat.RGBA32, false);
+        if (!texture.LoadImage(fileData))
+        {
+            Logger.LogWarning("Failed to decode texture!");
+            return;
+        }
+
+        texture.wrapMode = TextureWrapMode.Repeat;
+        texture.filterMode = FilterMode.Bilinear;
+        NewTexture = texture;
+
+        // LOG COMPLETION!
+        
+        Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+    }
+}
