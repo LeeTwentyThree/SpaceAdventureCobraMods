86c6c87a45c7dd653ef310233d51f7d9ace0b4a5
Lee23 <31892011+LeeTwentyThree@users.noreply.github.com>
2025-10-15 20:34:57 -0400

    Add 1982 anime sound patches project

---
+++ Cobra1982AnimeSoundPatches/Patches.cs
@@ -0,0 +1,20 @@
+﻿using HarmonyLib;
+
+namespace Cobra1982AnimeSoundPatches;
+
+[HarmonyPatch]
+public static class Patches
+{
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(NmiPatrouille), nameof(NmiPatrouille.Start))]
+    public static void ReplacePatrouilleSound(NmiPatrouille __instance)
+    {
+        if (__instance.m_SpecificShootProjectileClip != audioSelectionData.eCLIP.NMI_SHOOT_RIFLE)
+            return;
+        
+        if (CobraSoundReplacer.API.CustomSoundUtils.TryGetEClip("1982_anime_NPC_shoot", out var clip))
+            __instance.m_SpecificShootProjectileClip = clip;
+        else
+            Plugin.Logger.LogError("Failed to find EClip for the new shoot sound!");
+    }
+}
---
+++ Cobra1982AnimeSoundPatches/Plugin.cs
@@ -0,0 +1,22 @@
+﻿using System.Reflection;
+using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+
+namespace Cobra1982AnimeSoundPatches;
+
+[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+public class Plugin : BaseUnityPlugin
+{
+    internal static new ManualLogSource Logger;
+    
+    private void Awake()
+    {
+        Logger = base.Logger;
+
+        var assembly = Assembly.GetExecutingAssembly();
+        Harmony.CreateAndPatchAll(assembly);
+
+        Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+    }
+}
