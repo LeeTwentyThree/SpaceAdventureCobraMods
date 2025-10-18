9512a45df7b693e7d155242036015ae8461a7b02
Lee23 <31892011+LeeTwentyThree@users.noreply.github.com>
2025-10-17 22:13:55 -0400

    Add no super shot popup mod

---
+++ NoSuperShotPopup/Patches.cs
@@ -0,0 +1,15 @@
+﻿using HarmonyLib;
+
+namespace NoSuperShotPopup;
+
+[HarmonyPatch]
+public static class Patches
+{
+    [HarmonyPrefix]
+    [HarmonyPatch(typeof(UIController), nameof(UIController.UIShowNotificationUltraEnergy))]
+    public static bool RemoveUltraEnergyNotification(ref bool __result)
+    {
+        __result = true;
+        return false;
+    }
+}
