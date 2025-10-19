605cc412545859d357972d6bf6a4d5cb55e8bdef
Lee23 <31892011+LeeTwentyThree@users.noreply.github.com>
2025-10-18 23:10:44 -0400

    Fix spread shot soldier using wrong sounds

--- Cobra1982AnimeSoundPatches/Patches.cs
+++ Cobra1982AnimeSoundPatches/Patches.cs
@@ -9,12 +9,25 @@ public static class Patches
     [HarmonyPatch(typeof(NmiPatrouille), nameof(NmiPatrouille.Start))]
     public static void ReplacePatrouilleSound(NmiPatrouille __instance)
     {
-        if (__instance.m_SpecificShootProjectileClip != audioSelectionData.eCLIP.NMI_SHOOT_RIFLE)
-            return;
-        
-        if (CobraSoundReplacer.API.CustomSoundUtils.TryGetEClip("1982_anime_NPC_shoot", out var clip))
-            __instance.m_SpecificShootProjectileClip = clip;
-        else
-            Plugin.Logger.LogError("Failed to find EClip for the new shoot sound!");
+        if (__instance.m_SpecificShootProjectileClip == audioSelectionData.eCLIP.NMI_SHOOT_RIFLE)
+        {
+            if (__instance.gameObject.name.StartsWith("NmiCrystalBoy_Light_ShotBasic"))
+            {
+                if (CobraSoundReplacer.API.CustomSoundUtils.TryGetEClip("1982_anime_NPC_shoot", out var clip))
+                    __instance.m_SpecificShootProjectileClip = clip;
+                else
+                    Plugin.Logger.LogError("Failed to find EClip for the new shoot sound!");
+            }
+        }
+        else if (__instance.m_SpecificShootProjectileClip == audioSelectionData.eCLIP.NMI_SHOOT_MULTI)
+        {
+            if (__instance.gameObject.name.StartsWith("NmiCrystalBoy_Elite_ShotSpread"))
+            {
+                if (CobraSoundReplacer.API.CustomSoundUtils.TryGetEClip("1982_anime_NPC_multi_shoot", out var clip))
+                    __instance.m_SpecificShootProjectileClip = clip;
+                else
+                    Plugin.Logger.LogError("Failed to find EClip for the new multi shoot sound!");
+            }
+        }
     }
 }
