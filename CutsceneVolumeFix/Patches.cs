6e32de21725d957b0ec1f26ffeef9718e28c6003
Lee23 <31892011+LeeTwentyThree@users.noreply.github.com>
2025-11-04 02:32:42 -0500

    Add cutscene volume fix mod

---
+++ CutsceneVolumeFix/Patches.cs
@@ -0,0 +1,38 @@
+﻿using HarmonyLib;
+using UnityEngine;
+using UnityEngine.Video;
+
+namespace CutsceneVolumeFix;
+
+[HarmonyPatch]
+public static class Patches
+{
+    private static bool _overriding;
+    private static float _lastMusicVolume;
+
+    [HarmonyPrefix]
+    [HarmonyPatch(typeof(CobraVideoPlayer), nameof(CobraVideoPlayer.Play), typeof(VideoClip), typeof(GameObject), typeof(Token))]
+    public static void OnCutsceneStart()
+    {
+        if (!_overriding)
+        {
+            _lastMusicVolume = LoadSaveController.Instance.PreferencesData.volumes[2];
+            _overriding = true;
+        }
+        
+        var sfxVolume = AudioController.Instance.mixageGal[0];
+        AudioController.Instance.SetMusicGlobalVolume(sfxVolume);
+        LoadSaveController.Instance.PreferencesData.volumes[2] = sfxVolume;
+    }
+
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(CobraVideoPlayer), nameof(CobraVideoPlayer.Stop))]
+    public static void OnCutsceneStop()
+    {
+        if (!_overriding)
+            return;
+        AudioController.Instance.SetMusicGlobalVolume(_lastMusicVolume);
+        LoadSaveController.Instance.PreferencesData.volumes[2] = _lastMusicVolume;
+        _overriding = false;
+    }
+}
