fa1f0bc7ce1985afbe74b9d990bf9b038b4b029b
Lee23 <31892011+LeeTwentyThree@users.noreply.github.com>
2025-11-10 20:27:53 -0500

    Fix death sounds muffling other SFX

---
+++ NoAudioMuffling/TestPatch.cs
@@ -0,0 +1,17 @@
+﻿using HarmonyLib;
+
+namespace NoAudioMuffling;
+
+[HarmonyPatch]
+public static class TestPatch
+{
+    [HarmonyPrefix]
+    [HarmonyPatch(typeof(CAudio), nameof(CAudio.play), typeof(string), typeof(float), typeof(float), typeof(CAudio.eVolumeType))]
+    public static void TestPrefix(string clipName, ref CAudio.eVolumeType forceType)
+    {
+        if (!string.IsNullOrEmpty(clipName) && clipName.StartsWith("vox_SYS_BARKS_DAMAGE"))
+        {
+            forceType = CAudio.eVolumeType.sound;
+        }
+    }
+}
